cd465bde16b627fec0f82e0c6f5e8449813a1e34
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-08-22 23:58:25 +0700

    change private key

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 import "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCkslkWg1xte5zhKwE-ef_LRoFBN7Rlqyw",
-  authDomain: "facebook-1f413.firebaseapp.com",
-  projectId: "facebook-1f413",
-  storageBucket: "facebook-1f413.appspot.com",
-  messagingSenderId: "1069520425903",
-  appId: "1:1069520425903:web:ccd7e58d93bb389644155a",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const app = !firebase.apps.length
